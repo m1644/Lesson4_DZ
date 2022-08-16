8592a470a374618a565102152e1a85874a54869c
MaxX <maxx-f@mail.ru>
2022-08-16 11:53:01 +0300

    Add DZ_002

---
+++ DZ_002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма цифр в числе: {sumNumber}");
